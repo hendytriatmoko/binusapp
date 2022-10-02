73107ff6510bebe112044cc67ef547233542004c
rafipandyaa <mrafipandyaa@gmail.com>
2022-10-02 11:45:22 +0700

    text file

---
+++ pages/dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+      <textarea  class="text-area" v-model="text"></textarea>
+      <br />
+      <text-reader @load="text = $event"></text-reader>
+    </div>
+  </template>
+  
+  <script>
+ import TextReader from '~/components/TextReader.vue';
+  
+  export default {
+    name: "app",
+    data: () => ({ text: "" }),
+    components: {
+        TextReader,
+    },
+  };
+  </script>
+
+<style>
+    .text-area {
+      padding: 10px 10px 20px 10px;
+      margin: 30px;
+      width: 200px;
+    }
+    </style>
+    
+  
